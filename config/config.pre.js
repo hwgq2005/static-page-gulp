3960c44af02f3df163d12e3a063ffbe364abbd77
huangwengen <262281610@qq.com>
2020-02-01 20:39:21 +0800

    Feat: 新增预发布环境

---
+++ config/config.pre.js
@@ -0,0 +1,8 @@
+var env = {
+    actHost: 'https://preact.zhaoliangji.com',
+    proHost: 'https://preproduct.zhaoliangji.com',
+    phost: 'https://prepanda.huodao.hk',
+    staticHost: 'https://prefrontstatic.zhaoliangji.com'
+};
+
+module.exports = env;
